a1c7f837d8866896b1f6325987acb9b4b423123d
wangzq6886 <wangzq6886@qq.com>
2017-03-10 17:24:30 +0800

    system test 02

---
+++ php0001.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-03-10
+ * Time: 17:14
+ */
+    $name = 'hello world';
+    echo $name;
+
+    $name = 12345;
+    var_dump($name);
